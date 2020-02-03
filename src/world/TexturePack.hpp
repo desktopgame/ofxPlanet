a76c6f81dbd0ebfa132e9ed2be9ff520c3c5466d
desktopgame <koya0306@outlook.jp>
2020-02-03 11:59:03 +0900

    コメントを追加

--- src/world/TexturePack.hpp
+++ src/world/TexturePack.hpp
@@ -13,27 +13,90 @@
 namespace ofxPlanet {
 
 class Plane;
+/**
+ * TexturePack is list a texture collection.
+ * using for exchange for block textures.
+ */
 class TexturePack : public std::enable_shared_from_this<TexturePack> {
        public:
+		/**
+		 * add TexturePack by specific texture collection.
+		 * @param textureInfoCollection
+		 */
         static std::shared_ptr<TexturePack> load(
             const TextureInfoCollection& textureInfoCollection);
+		/**
+		 * add TexturePack.
+		 * @param baseDirectory
+		 * @param name
+		 */
         static std::shared_ptr<TexturePack> make(
             const std::string& baseDirectory, const std::string& name);
+		/**
+		 * returns a selecting TexturePack.
+		 * @return
+		 */
         static std::shared_ptr<TexturePack> getCurrent();
 
+		/**
+		 * add TextureSet.
+		 * @param set
+		 */
         void addTextureSet(const TextureSet& set);
+
+		/**
+		 * find texture index from name.
+		 * @param name
+		 * @return
+		 */
         int getTextureSetIndexForName(const std::string& name) const;
+
+		/**
+		 * returns texture from index.
+		 * @param index
+		 * @return
+		 */
         const TextureSet& getTextureSet(int index) const;
+
+		/**
+		 * remove a texture for specific index.
+		 */
         void removeTextureSet(int index);
+
+		/**
+		 * returns count a textures.
+		 * @return
+		 */
         int getTextureSetCount() const;
 
+		/**
+		 * returns texture from name.
+		 * @param name
+		 * @return
+		 */
         std::shared_ptr<Texture> getImage(const std::string& name) const;
 
+		/**
+		 * return true if selected.
+		 */
         bool isSelected() const;
+		/**
+		 * select this TexturePack.
+		 */
         void select();
+		/**
+		 * resolve a mapping for block and texture.
+		 */
         void resolve();
 
+		/**
+		 * returns a baseDirectory for using load texture.
+		 * @return
+		 */
         std::string getBaseDirectory() const;
+		/**
+		 * returns a baseDirectory as absolute path for using load texture.
+		 */
         std::string getBasePath() const;
 
        protected:
