90937b9c17edc3d6ade2b6b728b97b5b0e576c62
desktopgame <koya0306@outlook.jp>
2020-02-03 12:35:00 +0900

    コメントを追加

--- src/world/Planet.hpp
+++ src/world/Planet.hpp
@@ -13,18 +13,43 @@ class WorldGenerateService;
 class World;
 class Biome;
 class Camera;
-
+/**
+ * Planet is pair for World, and BlockTable.
+ */
 class Planet {
        public:
         explicit Planet(ofShader& shader);
-
+		/**
+		 * generate world.
+		 * @param size
+		 * @param biome
+		 */
         void generate(glm::vec3 size, std::shared_ptr<Biome> biome);
 
+		/**
+		 * update world.
+		 */
         void update();
+		/**
+		 * draw world to frame buffer object.
+		 */
         void drawToBuffer();
+
+		/**
+		 * draw frame buffer object to screen.
+		 */
         void render();
 
+		/**
+		 * returns world.
+		 * @return
+		 */
         std::shared_ptr<World> getWorld() const;
+
+		/**
+		 * returns blockTable.
+		 * @return
+		 */
         BlockTable getBlockTable() const;
 
        private:
