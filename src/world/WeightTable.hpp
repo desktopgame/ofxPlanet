8740a51080510261c7e198774c14d7f23ee6065b
desktopgame <koya0306@outlook.jp>
2020-02-03 11:44:22 +0900

    コメントを追加

--- src/world/WeightTable.hpp
+++ src/world/WeightTable.hpp
@@ -7,17 +7,53 @@
 #include "IntVec3Compare.hpp"
 
 namespace ofxPlanet {
+/**
+ * WeightTable is for storaging weighting.
+ */
 class WeightTable {
        public:
         explicit WeightTable(int xSize, int ySize, int zSize);
-
+		/**
+		 * add weight a specific position as center.
+		 * @param x
+		 * @param y
+		 * @param z
+		 * @param weight
+		 */
         void addWeight(int x, int y, int z, int weight);
-
+		/**
+		 * overwrite weight for specific position.
+		 * @param x
+		 * @param y
+		 * @param z
+		 * @param weight
+		 */
         void setWeight(int x, int y, int z, int weight);
+
+		/**
+		 * returns a weight for specific position.
+		 * @param x
+		 * @param y
+		 * @param z
+		 * @return
+		 */
         int getWeight(int x, int y, int z);
 
+		/**
+		 * return X-axis from weight table 3D size.
+		 * @return
+		 */
         int getXSize() const;
+
+		/**
+		 * return Y-axis from weight table 3D size.
+		 * @return
+		 */
         int getYSize() const;
+
+		/**
+		 * return Z-axis from weight table 3D size.
+		 */
         int getZSize() const;
 
        private:
