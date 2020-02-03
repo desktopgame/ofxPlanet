ff6cea6db7e89f24a499cbf492e05be8ce33f6e8
desktopgame <koya0306@outlook.jp>
2020-02-03 15:42:12 +0900

    コメントを削除

--- src/world/csvr/Parser.hpp
+++ src/world/csvr/Parser.hpp
@@ -11,36 +11,29 @@ constexpr char newline = '\n';
 using Line = std::vector<std::string>;
 using Table = std::vector<Line>;
 /**
- * csvr::Parser は CSV の独自拡張フォーマットをサポートするパーサです。
- * このパーサはCSVによって表される二次元配列の配列を保持します。
- * パース時には, 通常のCSV同様 ,(カンマ) をカラムの区切り文字として使用する他、
- * なにもない空行を CSV１つの終了 とみなします。
+ * Parser.
  */
 class Parser {
        public:
         explicit Parser();
 
         /**
-         * 指定のCSVRフォーマットの文字列を読み込んで、
-         * テーブルとして追加します。
+         * @param source
          */
         void parse(const std::string& source);
 
         /**
-         * 指定位置のテーブルを返します。
          * @param index
          * @return
          */
         Table& getTableAt(int index);
 
         /**
-         * 指定位置のテーブルを返します。
          * @param index
          * @return
          */
         const Table& getTableAt(int index) const;
         /**
-         * テーブルの数を返します。
          * @return
          */
         int getTableCount() const;
