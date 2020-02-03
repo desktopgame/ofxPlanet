624e8b9daae765b030442aef1dc411545b569e91
desktopgame <koya0306@outlook.jp>
2020-02-03 11:48:45 +0900

    コメントを追加

--- src/world/TextureSet.hpp
+++ src/world/TextureSet.hpp
@@ -8,22 +8,50 @@ namespace ofxPlanet {
 
 class TexturePack;
 class Texture;
+/**
+ * TextureSet is textures for side from blocks.
+ */
 class TextureSet {
        public:
         explicit TextureSet(const std::weak_ptr<const TexturePack> pack,
                             std::string name);
+		/**
+		 * returns a texture name.
+		 * @return
+		 */
         std::string getName() const;
 
+		/**
+		 * return a top-side texture.
+		 * @return
+		 */
         std::shared_ptr<Texture> getTopImage() const;
 
+		/**
+		 * return a bottom side texture.
+		 * @return
+		 */
         std::shared_ptr<Texture> getBottomImage() const;
 
+		/**
+		 * return a front side texture.
+		 * @return
+		 */
         std::shared_ptr<Texture> getFrontImage() const;
 
+		/**
+		 * return a back side texture.
+		 */
         std::shared_ptr<Texture> getBackImage() const;
 
+		/**
+		 * return a left side texture.
+		 */
         std::shared_ptr<Texture> getLeftImage() const;
 
+		/**
+		 * return a right side texture.
+		 */
         std::shared_ptr<Texture> getRightImage() const;
 
         std::string top, bottom, front, back, left, right;
