b756d7df548b63b59f05c5ede9ff44a1b3d30deb
desktopgame <koya0306@outlook.jp>
2020-02-03 13:10:54 +0900

    コメントを追加

--- src/world/BlockPack.hpp
+++ src/world/BlockPack.hpp
@@ -8,20 +8,71 @@
 
 namespace ofxPlanet {
 class Block;
+/**
+ * BlockPack is list a block collection.
+ */
 class BlockPack {
        public:
+		/**
+		 * add BlockPack by specific block collection.
+		 * @param blockInfoCollection
+		 * @return
+		 */
         static std::shared_ptr<BlockPack> load(
             const BlockInfoCollection& blockInfoCollection);
+		/**
+		 * add BlockPack.
+		 * @paam name
+		 * @return
+		 */
         static std::shared_ptr<BlockPack> make(const std::string& name);
+		/**
+		 * returns a selecting TexturePack.
+		 * @return
+		 */
         static std::shared_ptr<BlockPack> getCurrent();
 
+		/**
+		 * add Block.
+		 * @param block
+		 */
         void addBlock(const std::shared_ptr<Block>& block);
+
+		/**
+		 * find block index from name.
+		 * @param name
+		 * @return
+		 */
         int getBlockIndexForName(const std::string& name) const;
+
+		/**
+		 * returns block from index.
+		 * @param index
+		 * @return
+		 */
         std::shared_ptr<Block> getBlock(int index) const;
+
+		/**
+		 * returns block from name.
+		 * @param name
+		 * @return
+		 */
         std::shared_ptr<Block> getBlock(const std::string& name) const;
+
+		/**
+		 * returns count of blocks.
+		 * @return
+		 */
         int getBlockCount() const;
 
+		/**
+		 * return true if selected.
+		 * @return
+		 */
         bool isSelected() const;
+		/**
+		 * select this BlockPack.
+		 */
         void select();
 
        protected:
