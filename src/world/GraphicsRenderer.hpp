4426022a868c949de2bccfa70d39374f050ebb37
desktopgame <koya0306@outlook.jp>
2020-02-03 12:45:23 +0900

    コメントを追加

--- src/world/GraphicsRenderer.hpp
+++ src/world/GraphicsRenderer.hpp
@@ -9,18 +9,73 @@
 namespace ofxPlanet {
 class World;
 class GraphicsBatch;
+/**
+ * GraphicsRenderer is wrapper for GraphicsBatch.
+ */
 class GraphicsRenderer {
        public:
         GraphicsRenderer(const World& world, ofShader& shader);
 
+		/**
+		 * add position to GraphicsBatch.
+		 * @param texture
+		 * @param x
+		 * @param y
+		 * @param z
+		 */
         void putFront(GLuint texture, int x, int y, int z);
+		/**
+		 * add position to GraphicsBatch.
+		 * @param texture
+		 * @param x
+		 * @param y
+		 * @param z
+		 */
         void putBack(GLuint texture, int x, int y, int z);
+		/**
+		 * add position to GraphicsBatch.
+		 * @param texture
+		 * @param x
+		 * @param y
+		 * @param z
+		 */
         void putLeft(GLuint texture, int x, int y, int z);
+		/**
+		 * add position to GraphicsBatch.
+		 * @param texture
+		 * @param x
+		 * @param y
+		 * @param z
+		 */
         void putRight(GLuint texture, int x, int y, int z);
+		/**
+		 * add position to GraphicsBatch.
+		 * @param texture
+		 * @param x
+		 * @param y
+		 * @param z
+		 */
         void putTop(GLuint texture, int x, int y, int z);
+		/**
+		 * add position to GraphicsBatch.
+		 * @param texture
+		 * @param x
+		 * @param y
+		 * @param z
+		 */
         void putBottom(GLuint texture, int x, int y, int z);
+
+		/**
+		 * clear all vertex.
+		 */
         void clear();
+		/**
+		 * update all plane.
+		 */
         void update();
+		/**
+		 * draw all GraphicsBatch.
+		 */
         void render();
 
        protected:
