bcd290aa2f85f81fe2dacb283f90a46fa905463d
desktopgame <koya0306@outlook.jp>
2020-02-03 13:24:18 +0900

    コメントを追加

--- src/world/biome/BasicBiome.hpp
+++ src/world/biome/BasicBiome.hpp
@@ -12,6 +12,9 @@
 namespace ofxPlanet {
 
 class Block;
+/**
+ * BasicBiome is base class for implement terrain generation by perlin noise.
+ */
 class BasicBiome : public Biome {
        public:
         explicit BasicBiome();
@@ -19,17 +22,58 @@ class BasicBiome : public Biome {
         void generate(BlockTable& blockTable) override;
 
        protected:
+		/**
+		 * return true, if using callbacks.
+		 */
         virtual bool isUseCallbacks();
+		/**
+		 * called on after generate perlin noise.
+		 * filtering a got y-axis noise
+		 * @param y
+		 * @return
+		 */
         virtual float onFixHeight(float y);
+		/**
+		 * called on begin world generate.
+		 * @param blockTable
+		 */
         virtual void onBeginGenerateWorld(BlockTable& blockTable);
+		/**
+		 * called on end world generate.
+		 * @param blockTable.
+		 */
         virtual void onEndGenerateWorld(BlockTable& blockTable);
 
+		/**
+		 * called on begin terrain generate.
+		 */
         virtual void onBeginGenerateTerrain();
+		/**
+		 * called on after generate perlin noise.
+		 * @param blockTable
+		 * @param x
+		 * @param y
+		 * @param z
+		 */
         virtual void onGenerateTerrain(BlockTable& blockTable, int x, int y,
                                        int z);
+		/**
+		 * called on after generate terrain.
+		 */
         virtual void onEndGenerateTerrain();
-
+		/**
+		 * called on after generate terrain.
+		 * @param blockTable
+		 */
         virtual void onGenerateStructures(BlockTable& blockTable);
+		/**
+		 * called on after generate structure.
+		 * @param blockTable
+		 * @param x
+		 * @param y
+		 * @param z
+		 * @param noise
+		 */
         virtual void onGenerateCave(BlockTable& blockTable, int x, int y, int z,
                                     float noise);
         virtual void generateStructure(BlockTable& blockTable, MultiBlock mb,
--- src/world/biome/Biome.hpp
+++ src/world/biome/Biome.hpp
@@ -7,10 +7,17 @@ namespace ofxPlanet {
 
 class BlockTable;
 class World;
+/**
+ * Biome is terrain generation algorithm in world.
+ */
 class Biome {
        public:
         Biome() = default;
         virtual ~Biome() = default;
+		/**
+		 * write block data to block table.
+		 * @param blockTable
+		 */
         virtual void generate(BlockTable& blockTable) = 0;
 
        private:
