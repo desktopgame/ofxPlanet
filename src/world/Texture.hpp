8007e4fd35d6e1904302d9d47b7f182b89b94fb0
desktopgame <koya0306@outlook.jp>
2020-02-03 12:09:43 +0900

    コメントを追加

--- src/world/Texture.hpp
+++ src/world/Texture.hpp
@@ -7,22 +7,52 @@
 
 namespace ofxPlanet {
 /**
- * Texture �́A�e�N�X�`����OpenGL�ƕR�t����N���X�ł��B
+ * Texture is wrapper for OpenGL texture function.
  */
 class Texture {
        public:
         explicit Texture();
         ~Texture();
+		/** 
+		 * load texture file for specific path.
+		 * @param path
+		 */
         void load(const std::string& path);
-
+		/**
+		 * bind this texture.
+		 */
         void bind();
+		/**
+		 * unbind texture.
+		 */
         void unbind();
 
+		/**
+		 * returns width of texture.
+		 * @return
+		 */
         int getWidth() const;
+		/**
+		 * returns height of texture.
+		 * @return
+		 */
         int getHeight() const;
-
+		/**
+		 * returns RGBA data for texture.
+		 * @return
+		 */
         unsigned char* getData() const;
+
+		/**
+		 * returns texture object in OpenGL.
+		 * @return
+		 */
         GLuint getName() const;
+
+		/**
+		 * returns path for texture.
+		 * @return
+		 */
         std::string getPath() const;
 
        private:
