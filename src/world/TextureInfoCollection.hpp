042718d1a3b86a8d676126a18bf4d96c6a523fc5
desktopgame <koya0306@outlook.jp>
2020-02-03 12:05:27 +0900

    コメントを追加

--- src/world/TextureInfoCollection.hpp
+++ src/world/TextureInfoCollection.hpp
@@ -7,33 +7,77 @@
 #include "Optional.hpp"
 
 namespace ofxPlanet {
+/**
+ * TextureMappingRule is sides in block mapped texture.
+ */
 struct TextureMappingRule {
         explicit TextureMappingRule() = default;
         Optional<std::string> all, top, bottom, left, right, front, back;
 };
-
+/**
+ * TextureInfo is texture information for one blocks.
+ */
 struct TextureInfo {
         explicit TextureInfo() = default;
         std::string baseFileName;
         std::string reference;
         TextureMappingRule mappingRule;
 };
-
+/**
+ * TextureInfoCollection is collection of TextureInfo.
+ */
 class TextureInfoCollection {
        public:
         explicit TextureInfoCollection(const std::string& baseDirectory);
         explicit TextureInfoCollection();
 
+		/**
+		 * serialize as json.
+		 * @return
+		 */
         std::string serialize() const;
+		/**
+		 * deserialize from json.
+		 * @param json
+		 */
         void deserialize(const std::string& json);
-
+		/**
+		 * add TextureInfo.
+		 * @param textureInfo
+		 */
         void addTextureInfo(const TextureInfo& textureInfo);
+
+		/**
+		 * returns a TextureInfor from index.
+		 * @param index
+		 * @return
+		 */
         TextureInfo getTextureInfo(int index) const;
+
+		/**
+		 * return true if found TextureInfo from reference.
+		 * @param reference
+		 * @param outTextureInfo
+		 * @return
+		 */
         bool tryGetTextureInfo(const std::string& reference,
                                TextureInfo& outTextureInfo);
+		/**
+		 * returns a count all TextureInfo.
+		 * @return
+		 */
         int getTextureInfoCount() const;
 
+		/**
+		 * returns a baseDirectory using for texture load.
+		 * @return
+		 */
         std::string getBaseDirectory() const;
+
+		/**
+		 * returns a baseDirectory as absolute path using for texture load.
+		 * @return
+		 */
         std::string getBasePath() const;
 
        private:
