441989131e5237cc3a76439cf0f90fc8b444ba80
desktopgame <koya0306@outlook.jp>
2020-02-03 13:06:27 +0900

    コメントを追加

--- src/world/BlockRenderer.hpp
+++ src/world/BlockRenderer.hpp
@@ -5,20 +5,60 @@
 
 namespace ofxPlanet {
 class World;
+/**
+ * BlockRenderer is renderer for rendering block.
+ */
 class BlockRenderer {
        public:
         explicit BlockRenderer(const World& world, ofShader& shader);
 
+		/**
+		 * clear all renderer.
+		 */
         void clear();
+		/**
+		 * update all renderer.
+		 */
         void update();
+		/**
+		 * draw all renderer.
+		 */
         void render();
 
+		/**
+		 * returns renderer for render a cube.
+		 * @return
+		 */
         CubeRenderer& getCubeRenderer();
+		/**
+		 * returns renderer for render a slab.
+		 * @return
+		 */
         CubeRenderer& getLeftSlabRenderer();
+		/**
+		 * returns renderer for render a slab.
+		 * @return
+		 */
         CubeRenderer& getRightSlabRenderer();
+		/**
+		 * returns renderer for render a slab.
+		 * @return
+		 */
         CubeRenderer& getFrontSlabRenderer();
+		/**
+		 * returns renderer for render a slab.
+		 * @return
+		 */
         CubeRenderer& getBackSlabRenderer();
+		/**
+		 * returns renderer for render a slab.
+		 * @return
+		 */
         CubeRenderer& getTopSlabRenderer();
+		/**
+		 * returns renderer for render a slab.
+		 * @return
+		 */
         CubeRenderer& getBottomSlabRenderer();
 
        private:
