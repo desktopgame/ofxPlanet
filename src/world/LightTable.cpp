784bef7d165036ab827ecfd9263d43eff72feec0
desktopgame <gnndr36@gmail.com>
2020-02-11 13:54:12 +0900

    LightTableを追加

---
+++ src/world/LightTable.cpp
@@ -0,0 +1,42 @@
+#include "LightTable.hpp"
+#include <stdexcept>
+namespace ofxPlanet {
+LightTable::LightTable(int xSize, int ySize, int zSize)
+ : xSize(xSize), ySize(ySize), zSize(zSize), vec() {
+	for (int x = 0; x < xSize; x++) {
+		std::vector<std::vector<int> > yline;
+		for (int y = 0; y < ySize; y++) {
+			std::vector<int> zline;
+			for (int z = 0; z < zSize; z++) {
+				zline.push_back(0);
+			}
+			yline.push_back(zline);
+		}
+		vec.push_back(yline);
+	}
+}
+void LightTable::setLight(int x, int y, int z, int light) {
+	verifyBrightness(light);
+	vec[x][y][z] = light;
+}
+int LightTable::getLight(int x, int y, int z) const {
+	return vec[x][y][z];
+}
+int LightTable::getXSize() const {
+	return xSize;
+}
+int LightTable::getYSize() const {
+	return ySize;
+}
+int LightTable::getZSize() const {
+	return zSize;
+}
+bool LightTable::testBrightness(int brightness) {
+	return brightness >= BRIGHTNESS_MIN && brightness <= BRIGHTNESS_MAX;
+}
+void LightTable::verifyBrightness(int brightness) {
+	if (!testBrightness(brightness)) {
+		throw std::logic_error("light level is must be included in 0~15");
+	}
+}
+}
---
+++ src/world/LightTable.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#ifndef WORLD_LIGHTTABLE_HPP
+#define WORLD_LIGHTTABLE_HPP
+#include <vector>
+
+namespace ofxPlanet {
+/**
+ * LightTable.
+ */
+class LightTable {
+public:
+	explicit LightTable(int xSize, int ySize, int zSize);
+
+	/**
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @param light
+	 */
+	void setLight(int x, int y, int z, int light);
+	/**
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @return
+	 */
+	int getLight(int x, int y, int z) const;
+
+	/**
+	 * @return
+	 */
+	int getXSize() const;
+	/**
+	 * @return
+	 */
+	int getYSize() const;
+	/**
+	 * @return
+	 */
+	int getZSize() const;
+
+	/**
+	 * @param brightness
+	 * @return
+	 */
+	static bool testBrightness(int brightness);
+
+	/**
+	 * @param brightness
+	 */
+	static void verifyBrightness(int brightness);
+
+	static constexpr int BRIGHTNESS_MIN = 0;
+	static constexpr int BRIGHTNESS_MAX = 15;
+	static constexpr char* BRIGHTNAME_UNIFORM_NAME = "uBrightness";
+	static constexpr float BLACK = 0.8f;
+private:
+	int xSize, ySize, zSize;
+	std::vector<std::vector<std::vector<int> > > vec;
+};
+}
+#endif
