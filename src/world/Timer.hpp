dfbaba1d9e3e2f4250b090ecb6c8673e009c25c2
desktopgame <gnndr36@gmail.com>
2020-03-04 20:41:21 +0900

    Timerクラスを追加

---
+++ src/world/Timer.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef WORLD_TIMER_HPP
+#define WORLD_TIMER_HPP
+#include <chrono>
+#include <sstream>
+
+namespace ofxPlanet {
+class Timer {
+public:
+	explicit Timer();
+
+	void start();
+	void end();
+	std::string str() const;
+private:
+	std::chrono::system_clock::time_point t0;
+	std::chrono::system_clock::time_point t1;
+	std::stringstream buf;
+};
+}
+#endif
